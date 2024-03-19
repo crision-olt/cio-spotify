b39ffc1ada9cec9401c099cb9a6eebf8c2d2650d
Cristian Ionut Olteanu <96696392+crision-olt@users.noreply.github.com>
2024-03-19 19:41:24 +0100

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [ react(), tailwind({ config: { applyBaseStyles: false } }), sitemap()],
+});
