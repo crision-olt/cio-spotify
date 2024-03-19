3646e9cd989c02c834ec1969d010b0929dfab169
Cristian Ionut Olteanu <96696392+crision-olt@users.noreply.github.com>
2024-03-19 22:32:35 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://crision-olt.github.io/',
+  base: '/cio-spotify/',
   integrations: [ react(), tailwind({ config: { applyBaseStyles: false } }), sitemap()],
 });
