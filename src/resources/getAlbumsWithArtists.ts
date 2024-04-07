6c31e4ee3e28c1ab0d503f6ae3981d8f3451beac
Cristian Ionut, Olteanu <crision.olt@gmail.com>
2024-04-07 19:43:44 +0200

    feat: define getAlbumsWithArtists

--- src/resources/getAlbumsWithArtists.ts
+++ src/resources/getAlbumsWithArtists.ts
@@ -2,14 +2,23 @@ import { db, eq, Album, Artist, Song, SongArtists } from 'astro:db';
 
 import type { AlbumWithArtists } from '#types/albumWithArtists.js';
 
-
 export const getAlbumsWithArtists = async (): Promise<AlbumWithArtists[]> => {
-    const result = await db
-  .select({id: Album.id,title: Album.title, cover: Album.cover, color: Album.color, artist: Artist.name})
-  .from(Album)
-  .groupBy(Album.id)
-  .innerJoin(Song, eq(Album.id, Song.albumId))
-  .innerJoin(SongArtists, eq(Song.id, SongArtists.songId))
-  .innerJoin(Artist, eq(SongArtists.artistId, Artist.id))
+  const select = {
+    id: Album.id,
+    title: Album.title,
+    cover: Album.cover,
+    color: Album.color,
+    artist: Artist.name
+
+  }
+
+  const result = await db
+    .select(select)
+    .from(Album)
+    .groupBy(Album.id)
+    .innerJoin(Song, eq(Album.id, Song.albumId))
+    .innerJoin(SongArtists, eq(Song.id, SongArtists.songId))
+    .innerJoin(Artist, eq(SongArtists.artistId, Artist.id));
+
   return result;
 };
